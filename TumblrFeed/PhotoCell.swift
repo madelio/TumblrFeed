d50446f960f83a556c6831ecefc5d4a837122239
Madel Asistio <madel.asistio@gmail.com>
2017-02-02 21:38:52 -0800

    first commit

---
+++ TumblrFeed/PhotoCell.swift
@@ -0,0 +1,27 @@
+//
+//  PhotoCell.swift
+//  TumblrFeed
+//
+//  Created by Madel Asistio on 2/2/17.
+//  Copyright © 2017 Madel Asistio. All rights reserved.
+//
+
+import UIKit
+
+class PhotoCell: UITableViewCell {
+    
+    
+    @IBOutlet weak var photoImageView: UIImageView!
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
